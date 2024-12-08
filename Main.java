1f03a1c9d62e0dd352caaa1fb1adec5b257cfc56
rainy1221 <rain102208@gmail.com>
2024-12-08 15:37:19 -0500

    e

--- Main.java
+++ Main.java
@@ -10,19 +10,82 @@ import java.io.FileNotFoundException;
 public class Main {
     private static File f;
     private static Scanner s;
-    public static void main(String[] args) throws fileNotFoundException {
+
+    public static void main(String[] args) throws FileNotFoundException {
         f = new File("words.txt");
 
         int twoLetter = twoLetterWords();
+        int longestWords = longestWords();
+        int numOfPalindromes = numOfPalindromes();
+
+        System.out.println(twoLetter);
+        System.out.println(longestWords);
+        System.out.println(numOfPalindromes);
+        s.close();
     }
 
-    public static int twoLetterWords() throws fileNotFoundException {
+    public static int twoLetterWords() throws FileNotFoundException {
         int count = 0;
         s = new Scanner(f);
-        while (s.hasNext()){
-            if (s.nextLine().length() == 2){
-                count ++;
+        while (s.hasNext()) {
+            if (s.nextLine().length() == 2) {
+                count++;
             }
         }
+        return count;
     }
+
+
+    public static int longestWords() throws FileNotFoundException {
+        int maxLength = 0;
+        int repeats = 0;
+        String targetString;
+        s = new Scanner(f);
+        while (s.hasNext()) {
+            targetString = s.nextLine();
+            if (targetString.length() > maxLength) maxLength = s.nextLine().length();
+
+        }
+        s.close();
+        s = new Scanner(f);
+        while (s.hasNext()) {
+            targetString = s.nextLine();
+            if (targetString.length() == maxLength) repeats++;
+        }
+        return repeats;
+    }
+
+    public static int numOfPalindromes() {
+        int palindromes = 0;
+
+        String targetString;
+        while (s.hasNext()) {
+            targetString = s.nextLine();
+            String reversedString = "";
+            for (int i = targetString.length() - 1; i > 0; i--) {
+                reversedString += targetString.charAt(i);
+            }
+            if (reversedString.equals(targetString)) palindromes++;
+        }
+        return palindromes;
+
+    }
+//
+//    int palindromes = 0;
+//
+//    String targetString;
+//    while(s.hasNext())
+//
+//    {
+//        targetString = s.nextLine();
+//        String reversedString = new StringBuilder(targetString).reverse().toString();
+//
+//        if (reversedString.equals(targetString)) {
+//            palindromes++;
+//        }
+//    }
+//    return palindromes;
+//}
+
+
 }
